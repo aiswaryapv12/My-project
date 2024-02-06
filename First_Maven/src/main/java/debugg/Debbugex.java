fb0695f9c3c15298dfc5e19a41c8609cbb407dbf
user <user@DESKTOP-39H5R20>
2024-02-06 18:14:12 +0530

    debbug ex commit

---
+++ First_Maven/src/main/java/debugg/Debbugex.java
@@ -0,0 +1,21 @@
+package debugg;
+
+
+
+public class Debbugex {
+	public void display()
+	{
+		for(int i =0; i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+	
+	
+	public static void main(String args[])
+	{
+		Debbugex obj = new Debbugex();
+		obj.display();
+		
+	}
+}
