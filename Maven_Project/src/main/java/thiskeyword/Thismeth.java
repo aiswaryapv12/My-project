204d74d631461d448de8e59f4025cb25ddeba401
user <user@DESKTOP-39H5R20>
2024-02-06 13:35:01 +0530

    this method

---
+++ Maven_Project/src/main/java/thiskeyword/Thismeth.java
@@ -0,0 +1,32 @@
+package thiskeyword;
+
+public class Thismeth {
+	int num;
+	int num2;
+	public Thismeth(int num,int num2)
+	{
+		this.num =num;
+		this.num2 = num2;
+		
+	}
+	public void display()
+	{
+		this.sub();
+		int  c = num+num2;
+		System.out.println("sum is "+c);
+	}
+	public void sub ()
+	{
+		int sub = num - num2;
+		System.out.println("substracte value is "+sub);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thismeth obj = new Thismeth(20,10);
+		obj.display();
+		
+
+	}
+
+}
