cce466eade19961057f6b67efcb7b27209617979
user <user@DESKTOP-39H5R20>
2024-02-08 15:28:53 +0530

    null pointer example

---
+++ Exception_Project/src/main/java/nullpointer/Nullexa.java
@@ -0,0 +1,20 @@
+package nullpointer;
+
+public class Nullexa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+	   try{    
+		      String s=null;  
+		      System.out.println(s.length());  
+		   }   
+	   catch(Exception e)  
+		   {  
+		      System.out.println( "Exception handled");  
+		    }             
+		         System.out.println("rest of the code");    
+		    }  
+		 
+	}
+
+
