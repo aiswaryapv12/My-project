8cec1f443ddab996794d92329056591565bcbdae
user <user@DESKTOP-39H5R20>
2024-02-15 23:23:18 +0530

    Array2d

---
+++ First_Maven/src/main/java/array2d/Array2d.java
@@ -0,0 +1,20 @@
+package array2d;
+
+public class Array2d {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		int arr[][] = {{2,3,5},{4,5,6}};
+		for(int i=0; i<2;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+			System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
